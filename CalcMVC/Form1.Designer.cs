756b986868388ea17cdd520b60985c0a1cafd9f8
Maxim Gilman <maxim.gilman@yandex.ru>
2017-06-07 21:40:47 +0500

    Add Enhanced Interface

--- CalcMVC/Form1.Designer.cs
+++ CalcMVC/Form1.Designer.cs
@@ -43,7 +43,7 @@
             this.ActionsWithTime = new System.Windows.Forms.ComboBox();
             this.Equal = new System.Windows.Forms.Button();
             this.Answer = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.ReverseButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // HoursTextBox1
@@ -52,6 +52,7 @@
             this.HoursTextBox1.Name = "HoursTextBox1";
             this.HoursTextBox1.Size = new System.Drawing.Size(100, 20);
             this.HoursTextBox1.TabIndex = 0;
+            this.HoursTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.HoursTextBox1_KeyPress);
             // 
             // MinutesTextBox1
             // 
@@ -59,6 +60,7 @@
             this.MinutesTextBox1.Name = "MinutesTextBox1";
             this.MinutesTextBox1.Size = new System.Drawing.Size(100, 20);
             this.MinutesTextBox1.TabIndex = 1;
+            this.MinutesTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MinutesTextBox1_KeyPress);
             // 
             // SecondsTextBox1
             // 
@@ -66,6 +68,7 @@
             this.SecondsTextBox1.Name = "SecondsTextBox1";
             this.SecondsTextBox1.Size = new System.Drawing.Size(100, 20);
             this.SecondsTextBox1.TabIndex = 2;
+            this.SecondsTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SecondsTextBox1_KeyPress);
             // 
             // HoursLabel1
             // 
@@ -127,6 +130,7 @@
             this.SecondsTextBox2.Name = "SecondsTextBox2";
             this.SecondsTextBox2.Size = new System.Drawing.Size(100, 20);
             this.SecondsTextBox2.TabIndex = 8;
+            this.SecondsTextBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SecondsTextBox2_KeyPress);
             // 
             // MinutesTextBox2
             // 
@@ -134,6 +138,7 @@
             this.MinutesTextBox2.Name = "MinutesTextBox2";
             this.MinutesTextBox2.Size = new System.Drawing.Size(100, 20);
             this.MinutesTextBox2.TabIndex = 7;
+            this.MinutesTextBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MinutesTextBox2_KeyPress);
             // 
             // HoursTextBox2
             // 
@@ -141,6 +146,7 @@
             this.HoursTextBox2.Name = "HoursTextBox2";
             this.HoursTextBox2.Size = new System.Drawing.Size(100, 20);
             this.HoursTextBox2.TabIndex = 6;
+            this.HoursTextBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.HoursTextBox2_KeyPress);
             // 
             // ActionsWithTime
             // 
@@ -170,29 +176,31 @@
             // 
             // Answer
             // 
+            this.Answer.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.Answer.AutoSize = true;
-            this.Answer.Location = new System.Drawing.Point(162, 250);
+            this.Answer.Location = new System.Drawing.Point(160, 250);
             this.Answer.Name = "Answer";
-            this.Answer.Size = new System.Drawing.Size(126, 13);
+            this.Answer.Size = new System.Drawing.Size(133, 13);
             this.Answer.TabIndex = 14;
-            this.Answer.Text = "FFFFFFFFFFFFFFasFFFF";
-            this.Answer.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Answer.Text = "Тут будет выведен ответ";
+            this.Answer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // button1
+            // ReverseButton
             // 
-            this.button1.Location = new System.Drawing.Point(181, 26);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 15;
-            this.button1.Text = "Реверс";
-            this.button1.UseVisualStyleBackColor = true;
+            this.ReverseButton.Location = new System.Drawing.Point(181, 26);
+            this.ReverseButton.Name = "ReverseButton";
+            this.ReverseButton.Size = new System.Drawing.Size(75, 23);
+            this.ReverseButton.TabIndex = 15;
+            this.ReverseButton.Text = "Реверс";
+            this.ReverseButton.UseVisualStyleBackColor = true;
+            this.ReverseButton.Click += new System.EventHandler(this.ReverseButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(443, 272);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ReverseButton);
             this.Controls.Add(this.Answer);
             this.Controls.Add(this.Equal);
             this.Controls.Add(this.ActionsWithTime);
@@ -209,7 +217,7 @@
             this.Controls.Add(this.MinutesTextBox1);
             this.Controls.Add(this.HoursTextBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "TimeCalculator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -232,7 +240,7 @@
         private System.Windows.Forms.ComboBox ActionsWithTime;
         private System.Windows.Forms.Button Equal;
         private System.Windows.Forms.Label Answer;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ReverseButton;
     }
 }
 
--- CalcMVC/Form1.cs
+++ CalcMVC/Form1.cs
@@ -10,31 +10,172 @@ using System.Windows.Forms;
 
 namespace CalcMVC
 {
+    
     //проверка гит
     public partial class Form1 : Form
     {
+        const int TextBoxesMaxLength = 7;  //макс. размер тексбокса  в символах
         public Form1()
         {
             InitializeComponent();
+            HoursTextBox2.MaxLength = TextBoxesMaxLength;
+            HoursTextBox1.MaxLength = TextBoxesMaxLength;
+            MinutesTextBox1.MaxLength = TextBoxesMaxLength;
+            MinutesTextBox2.MaxLength = TextBoxesMaxLength;
+            SecondsTextBox1.MaxLength = TextBoxesMaxLength;
+            SecondsTextBox2.MaxLength = TextBoxesMaxLength;
         }
-
+        /// <summary>
+        /// Подсчет значений и вывод на label
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Equal_Click(object sender, EventArgs e)
         {
+            switch (ActionsWithTime.Text)
 
+            {
+
+                case "+":
+                    {
+                        MessageBox.Show("Вывод Суммы");
+                        break;
+                    }
+                case "-":
+                    {
+                        MessageBox.Show("Вывод Разности");
+                        break;
+                    }
+                case "<=>":
+                    {
+                        MessageBox.Show("Вывод Промежутка");
+                        break;
+                    }
+                case "H":
+                    {
+                        MessageBox.Show("Вывод времени в часах");
+                        break;
+                    }
+                case "M":
+                    {
+                        MessageBox.Show("Вывод времени в минутах");
+                        break;
+                    }
+                case "S":
+                    {
+                        MessageBox.Show("Вывод времени в секундах");
+                        break;
+                    }
+            }
         }
 
+        /// <summary>
+        /// Вывод для форматов времени: H/M/S
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ActionsWithTime_SelectedIndexChanged(object sender, EventArgs e)
         {
 
             switch (ActionsWithTime.Text)
             {
-                case "+":
+                case "H":
                     {
-                       
+                        MessageBox.Show("Особый вывод для конвертирования в часы");
+                        break;
+                    }
+                case "M":
+                    {
+                        MessageBox.Show("Особый вывод для конвертирования в минуты");
+                        break;
+                    }
+                case "S":
+                    {
+                        MessageBox.Show("Особый вывод для конвертирования в секунды");
                         break;
                     }
             }
         }
+        /// <summary>
+        /// Кнопка смены времени из левых textBox'ов в правые. Для быстрого изменения.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ReverseButton_Click(object sender, EventArgs e)
+        {
+          
+            string ToSwipe = string.Empty;
+            /*Hours swipe*/
+            ToSwipe = HoursTextBox1.Text;
+            HoursTextBox1.Text = HoursTextBox2.Text;
+            HoursTextBox2.Text = ToSwipe;
+
+            /*Minutes swipe*/
+            ToSwipe = MinutesTextBox1.Text;
+            MinutesTextBox1.Text = MinutesTextBox2.Text;
+            MinutesTextBox2.Text = ToSwipe;
+
+            /*Seconds swipe*/
+            ToSwipe = SecondsTextBox1.Text;
+            SecondsTextBox1.Text = SecondsTextBox2.Text;
+            SecondsTextBox2.Text = ToSwipe;
+
+        }
+        /*  События нажатия клавиш. Запрет на !число + возможность нажать backspace*/
+        #region KeyPressesEvents
+        private void HoursTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar)&&e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void MinutesTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void SecondsTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsNumber(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void HoursTextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsNumber(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void MinutesTextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsNumber(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void SecondsTextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsNumber(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+        }
+        #endregion  
+
     }
    
 }
