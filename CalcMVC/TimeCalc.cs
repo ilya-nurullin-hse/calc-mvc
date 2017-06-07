251edff51d07d900ce3324c99d347b9cab5cf933
Ilya Nurullin <nurullinin@mail.ru>
2017-06-07 15:30:38 +0500

    Add outline of model TimeCalc

---
+++ CalcMVC/TimeCalc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcMVC
+{
+    class TimeCalc
+    {
+        public static Time Sum(Time first, Time second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Time Sub(Time first, Time second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Time GetInterval(Time from, Time to)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int ToSeconds(Time time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int ToMinutes(Time time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int ToHours(Time time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
